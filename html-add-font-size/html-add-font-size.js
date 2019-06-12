189f92b57eeaf9580ad2536a6b9df07acc72609d
liqiang <xmly@liqiang.local>
2019-06-12 11:34:42 +0800

    add html-add-font-size

---
+++ html-add-font-size/html-add-font-size.js
@@ -0,0 +1,29 @@
+const getOptions = require('loader-utils').getOptions
+
+module.exports = function (source) {
+    const options = getOptions(this) || {}
+    const string = `
+        <script>
+            var u = navigator.userAgent.toLowerCase()
+
+            var html = document.querySelector('html'),
+                clientWidth = html.clientWidth;
+            if (!/MicroMessenger/i.test(u)) {
+                // 非微信环境为2
+                window._currentEnv = 2
+                if (/mix 2 build/i.test(u)) {
+                    html.style.fontSize = clientWidth / ${ options['@A'] || 5 } / 1.3 + 'px'
+                } else if (/hwi-al00/i.test(u)) {
+                    html.style.fontSize = clientWidth / ${ options['@A'] || 5 } / 1.2 + 'px'
+                } else {
+                    html.style.fontSize = clientWidth / ${ options['@A'] || 5 } + 'px'
+                }
+            } else {
+                // 微信环境为1
+                window._currentEnv = 1
+                html.style.fontSize = clientWidth / ${ options['@A'] || 5 } + 'px'
+            }
+        </script>
+    `
+    return source.replace(/<head[^>]*>([\s\S]*)<\/head>/g, `$1${ string }`)
+}
