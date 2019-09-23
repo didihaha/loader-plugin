9b644b848aea6777c40105e0183e21f8ea75dc07
didihaha <158673384@qq.com>
2019-09-23 17:54:28 +0800

    更新插入文件插件入参

--- add-asset-to-html-plugin/add-asset-to-html-plugin.js
+++ add-asset-to-html-plugin/add-asset-to-html-plugin.js
@@ -1,38 +1,35 @@
-// 该段代码中的参数5后面要换为可配置项，并将addAssetHtmlPlugin的功能集成进来
-const string = `
-        <script>
-            var u = navigator.userAgent.toLowerCase()
-
-            var html = document.querySelector('html'),
-                clientWidth = html.clientWidth;
-            if (!/MicroMessenger/i.test(u)) {
-                // 非微信环境为2
-                window._currentEnv = 2
-                if (/mix 2 build/i.test(u)) {
-                    html.style.fontSize = clientWidth / 5 / 1.3 + 'px'
-                } else if (/hwi-al00/i.test(u)) {
-                    html.style.fontSize = clientWidth / 5 / 1.2 + 'px'
-                } else {
-                    html.style.fontSize = clientWidth / 5 + 'px'
-                }
-            } else {
-                // 微信环境为1
-                window._currentEnv = 1
-                html.style.fontSize = clientWidth / 5 + 'px'
-            }
-        </script>
-    `
-
 class TestPlugin {
+    constructor (options) {
+        this.options = options || []
+    }
     apply (compiler) {
         compiler.hooks.emit.tap('HtmlWebpackPlugin', compilation => {
             const htmlArr = Object.keys(compilation.assets).filter(html => /html$/.test(html))
             htmlArr.forEach(html => {
                 let htmlString = compilation.assets[html].source()
-                const res = htmlString.replace(/<head[^>]*>([\s\S]*)<\/head>/g, ($1, $2) => {
-                    return $1.replace($2, $2 + string)
+                this.options.reverse().forEach(option => {
+                    const { module, filePath, assetLocation = 'head' } = option
+                    let string = '', res = ''
+                    if (module) {
+                        if (typeof module === 'function') {
+                            string = option.module()
+                        } else {
+                            string = module
+                        }
+                    } else if (filePath) {
+
+                    }
+                    if (assetLocation === 'head') {
+                        res = htmlString.replace(/<head[^>]*>([\s\S]*)<\/head>/g, ($1, $2) => {
+                            return $1.replace($2, $2 + string)
+                        })
+                    } else if (assetLocation === 'frontOfOtherScripts') {
+                        res = htmlString.replace(/<body[^>]*>([\s\S]*)(<script[^>]*><\/script>)<\/body>/g, ($1, $2, $3) => {
+                            return $1.replace($3, string + $3)
+                        })
+                    }
+                    compilation.assets[html].source = () => res
                 })
-                compilation.assets[html].source = () => res
             })
         });
     }
